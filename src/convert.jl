a7ac715a05566af130d912356c61012eff5796b8
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-03-18 12:07:13 -0400

    document `convert.jl`

--- src/convert.jl
+++ src/convert.jl
@@ -1,9 +1,23 @@
 include("data.jl")
 
+"""
+Returns the first item of a vector if `multiple` is false, otherwise the vector.
+Arguments:
+    `to_give::Vector{String}`: The vector to return.
+    `multiple::Bool`: Whether to return the vector or the first item of the vector.
+Returns:
+    `::Vector{String}`: A one-item vector of the first item of `to_give` if `multiple` is true, otherwise `to_give`.
+"""
 function give(to_give::Vector{String}, multiple::Bool)::Vector{String}
     multiple ? to_give : [to_give[1]]
 end
 
+"""
+Convert a Hanzi reference string to pinyin given a conversion map.
+Arguments:
+    `reference::String`: The Hanzi string.
+    `conversions::Dict{String, Vector{String}}`: A dictionary mapping Hanzi to all possible pinyin representations.
+"""
 function to_pinyin(reference::String, conversions::Dict{String, Vector{String}}, multiple::Bool)::Vector{Vector{String}}
     unknown::String = ""
     elements::Vector{Vector{String}} = []
@@ -19,6 +33,16 @@ function to_pinyin(reference::String, conversions::Dict{String, Vector{String}},
     length(unknown) == 0 ? elements : push!(elements, [unknown])
 end
 
+"""
+Find the pinyin of a Hanzi reference string.
+Arguments:
+    `reference::String`: The Hanzi string.
+    Optional:
+    `multiple::Bool=false`: Whether to return multiple pronunciations for characters with multiple pronunciations.
+Returns:
+    `Vector{Vector{String}}`: If `multiple` is `false`, a vector of single-item vectors with Hanzi pronunciations.
+        If `multiple` is `true`, a vector of variable-length vectors, with each containing the possible pronunciations.
+"""
 function pinyin(reference::String; multiple::Bool=false)::Vector{Vector{String}}
     to_pinyin(reference, HANZI_TO_PINYIN, multiple)
 end
